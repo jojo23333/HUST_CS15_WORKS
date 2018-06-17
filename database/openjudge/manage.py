56114c444497551f26eeafa9c5cd6914d4352a25
jojo23333 <jonassenli@gmail.com>
2018-06-17 21:22:32 +0800

    start projrct

--- database/openjudge/manage.py
+++ database/openjudge/manage.py
@@ -3,7 +3,7 @@ import os
 import sys
 
 if __name__ == "__main__":
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "OpenJudge.settings")
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "openjudge.settings")
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
